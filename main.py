3aaa240e3690866bf559d0140ee39bbaa4101ea5
Nothing <egorgolubev47@gmail.com>
2025-02-20 18:54:01 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,9 +9,20 @@ def index():
 
 
 @app.route('/index')
-def about():
+def ad():
     return '<h2>И на Марсе будут яблони цвести!</h2>'
 
+@app.route('/promotion')
+def promotion():
+    return ('<p>Человечество вырастает из детства.</p>'
+            '<p>Человечеству мала одна планета.</p>'
+            '<p>Мы сделаем обитаемыми безжизненные пока планеты.</p>'
+            '<p>И начнем с Марса!</p>'
+            '<p>Присоединяйся!</p>')
+
+@app.route('/image_mars')
+def mission():
+    return render_template('image_mars.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
